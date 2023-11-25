4417bdab6db5f52d4d1cb6e99176d40d2d31dcd9
Dasha <dashavavilova2002@icloud.com>
2023-11-26 01:28:41 +0300

    add task 4 for homework 7

---
+++ src/test/java/edu/hw7/Task4Test.java
@@ -0,0 +1,54 @@
+package edu.hw7;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Task4Test {
+    @Test
+    void testPiCalculationAccuracy() {
+        int simulations = 1000000;
+        double calculatedPi = Task4.calculationOfPiUsingMonteCarloMethod(1, simulations);
+
+        double exactPi = Math.PI;
+
+        double tolerance = 0.01;
+
+        assertTrue(Math.abs(calculatedPi - exactPi) < tolerance);
+    }
+
+    @Test
+    void testPerformanceWithDifferentThreadCounts() {
+        int simulations = 10_000_000;
+
+        long startTimeSingleThread = System.nanoTime();
+        Task4.calculationOfPiUsingMonteCarloMethod(1, simulations);
+        long endTimeSingleThread = System.nanoTime();
+
+        long startTimeTwoThreads = System.nanoTime();
+        Task4.calculationOfPiUsingMonteCarloMethod(2, simulations);
+        long endTimeTwoThreads = System.nanoTime();
+
+        long startTimeFourThreads = System.nanoTime();
+        Task4.calculationOfPiUsingMonteCarloMethod(4, simulations);
+        long endTimeFourThreads = System.nanoTime();
+
+        System.out.println("Time spent on 1 thread and 10 million simulations: " +
+            (endTimeSingleThread - startTimeSingleThread) / 1_000_000 + " milliseconds");
+        System.out.println("Time spent on 2 threads and 10 million simulations: " +
+            (endTimeTwoThreads - startTimeTwoThreads) / 1_000_000 + " milliseconds");
+        System.out.println("Time spent on 4 threads and 10 million simulations: " +
+            (endTimeFourThreads - startTimeFourThreads) / 1_000_000 + " milliseconds");
+    }
+
+    @Test
+    void testAccuracyWithDifferentSimulations() {
+        int[] simulations = {10_000_000, 100_000_000, 1_000_000_000};
+
+        for (int simulation : simulations) {
+            double calculatedPi = Task4.calculationOfPiUsingMonteCarloMethod(4, simulation);
+            double exactPi = Math.PI;
+            double accuracy = (1 - Math.abs(calculatedPi - exactPi) / exactPi) * 100;
+            System.out.println(String.format("Accuracy in %d simulations: ", simulation) + accuracy);
+        }
+    }
+}
